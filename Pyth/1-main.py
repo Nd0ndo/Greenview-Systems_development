b3b5d28e901018acfb26df1bdb4d9410d708ac17
Nd0ndo <ndondompinga01@gmail.com>
2024-02-07 12:17:15 +0200

    Representation of a car in a car lot Test

---
+++ Pyth/1-main.py
@@ -0,0 +1,7 @@
+class car : 
+    """Represents a car in a car lot"""
+    def..init..(self, make, model, year, price_usd):
+        Self.make = make
+        Self.model = model
+        Self.year = year
+        Self.price_usd = price_usd
---
+++ Pyth/4-main.py
@@ -0,0 +1,7 @@
+class car:
+    """Represents a car in a car lot"""
+    def__init__(self, make, model, year, price_usd):
+        Self.make = make
+        Self.model = model
+        Self.year = year
+        Self.price_usd
